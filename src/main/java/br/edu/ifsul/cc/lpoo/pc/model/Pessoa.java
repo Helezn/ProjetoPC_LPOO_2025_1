0898a765ea639c53ffc8ff37ac5e0d9d74872ba9
helenzn <helenzn21@gmail.com>
2025-03-10 11:38:56 -0300

    implementação dia 10.03.25

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/pc/model/Pessoa.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.cc.lpoo.pc.model;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author acer
+ */
+public abstract class Pessoa {
+    
+    private String cpf;
+    private String rg;
+    private String nome;
+    private String senha;
+    private Calendar data_nascimento;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private Boolean status;
+           
+    public Pessoa(){
+//        String cpf, String rg, String nome, String senha, Calendar data_nascimento, String cep, String numero, String complemento, Boolean status
+//        this.cpf = cpf;
+//        this.nome = nome;
+//        this.telefone = telefone;
+//        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Calendar getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Calendar data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+    
+    
+}
